9d4efe91448822a6a77ab774e8fc8b6d28e82d87
Seba G <sebag.git@gmail.com>
2022-04-10 21:52:32 +0200

    Find normalized_bufname with plain flag set to true - fix the problem with dashes in filename

--- lua/omnisharp_extended/utils.lua
+++ lua/omnisharp_extended/utils.lua
@@ -36,7 +36,7 @@ U.get_or_create_buf = function(name)
     -- in buffer name. On Windows nvim_buf_set_name might change the buffer name and include some stuff before.
     if string.find(name, '^/%$metadata%$/.*$') then
       local normalized_bufname = string.gsub(bufname, '\\', '/')
-      if string.find(normalized_bufname, name) then
+      if string.find(normalized_bufname, name, 0, true) then
         return buf
       end
     else
